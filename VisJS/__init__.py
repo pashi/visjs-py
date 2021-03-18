b5c123d3ee92a1056db034d51c61fb5420e22c1b
Pasi Lammi <pasi.lammi@enfogroup.com>
2021-03-18 15:27:21 +0200

    python3 compatible

--- VisJS/__init__.py
+++ VisJS/__init__.py
@@ -22,11 +22,11 @@ class VisJS:
     ret = []
     for entry in self.nodes:
         r = {}
-        for k,v in self.nodes[entry].iteritems():
-            r[k] = v
-        if r.has_key('image'):
+        for k in self.nodes[entry]:
+            r[k] = self.nodes[entry][k]
+        if 'image' in r:
             r["shape"] = "image"
-        if r.has_key('name'):
+        if 'name' in r:
             del r['name']
         ret.append(r)
     return json.dumps(ret, indent=2)
@@ -39,18 +39,19 @@ class VisJS:
     return json.dumps(ret, indent=2)
 
   def add_node(self,data):
-    if not self.nodes.has_key(data['name']):
+    if not data['name'] in self.nodes:
         name = data['name']
         self.nodes[name] = {}
-        for k, v in data.iteritems():
-            self.nodes[name][k] = v
-        if not self.nodes[name].has_key('id'):
+        #for k, v in data.iteritems():
+        for k in data:
+            self.nodes[name][k] = data[k]
+        if not 'id' in self.nodes[name]:
             self.nodes[name]['id'] = self.get_id()
 
   def add_edge(self,s,d):
     src = s['name']
     dst = d['name']
-    if self.nodes.has_key(src) and self.nodes.has_key(dst):
+    if src in self.nodes and dst in self.nodes:
         self.edges.append({'src': self.nodes[src]['id'], 'dst': self.nodes[dst]['id'] })
 
   def read_template(self,filename):
@@ -63,4 +64,4 @@ class VisJS:
     e = self.json_edges()
     t = self.title
     data = { 'nodes': n, 'edges': e, 'title': t }
-    print self.template % data
+    print (self.template % data)
